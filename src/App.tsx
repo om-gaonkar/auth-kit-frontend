7042ed02068ec4b8a9dfecf9848bc874594f8036
om-gaonkar <omgaonkar999@gmail.com>
2026-08-24 19:11:18 +0530

    auth-kit-fontend initialized

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="flex justify-center items-center min-h-screen bg-gray-700">
+        Project Initialized
+      </h1>
+    </div>
+  );
+}
+
+export default App;
